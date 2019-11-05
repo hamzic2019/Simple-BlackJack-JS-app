f9e3c5b8dfa8799a51e7f4fd97d5324f8268276f
hamzic2019 <hamzicha@hotmail.com>
2019-11-05 22:38:44 +0100

    a bit update of app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-dfghjk,
+//
+// Haris Hamzić
+// BlackJack Console Based Game
+// 05.11.2019
+//
+
+let card1 = 'Ace of Spades',
+    card2 = 'Ten of Hearts';
+
+
+
+console.log("Welcome to BlackJack Console Based Game");
+
+console.log("You are dealt: ");
+console.log(" " + card1);
+console.log(" " + card2);
